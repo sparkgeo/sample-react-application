7d00ef9c586f645ba7d4c2e4556674dff13a878e
Brian Bancroft <hello@brianbancroft.ca>
2019-11-12 15:45:51 -0800

    🔧  Working tests for helpers as well as components

--- src/components/SampleWidget/SampleWidget.test.js
+++ src/components/SampleWidget/SampleWidget.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from '@testing-library/react'
 import SampleWidget from './index'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
+test('renders without crashing', async () => {
+  const { getAllByText } = render(<SampleWidget />)
 
-  ReactDOM.render(<SampleWidget />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  const matches = await getAllByText("It's a widget!")
+
+  expect(matches.length).toBe(1)
 })
